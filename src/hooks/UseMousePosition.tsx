74e9379f6590cb866dd1cca11e91169567314432
raveendrahewage <hewagerv@gmail.com>
2023-03-10 09:38:06 +0530

    touchmove added

--- src/hooks/UseMousePosition.tsx
+++ src/hooks/UseMousePosition.tsx
@@ -1,20 +1,28 @@
 import { useEffect, useState } from "react";
-const useMousePosition=()=> {
-  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+const useMousePosition = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const mouseMoveHandler = (event: MouseEvent) => {
+    const { clientX, clientY } = event;
+    setMousePosition({ x: clientX, y: clientY });
+  };
+
+  const touchMove = (event: TouchEvent) => {
+    const { pageX, pageY } = event.changedTouches[0];
+    setMousePosition({ x: pageX, y: pageY });
+  };
 
   useEffect(() => {
-    const mouseMoveHandler = (event: { clientX: any; clientY: any; }) => {
-      const { clientX, clientY } = event;
-      setMousePosition({ x: clientX, y: clientY });
-    };
     document.addEventListener("mousemove", mouseMoveHandler);
+    document.addEventListener("touchmove", touchMove);
 
     return () => {
       document.removeEventListener("mousemove", mouseMoveHandler);
+      document.removeEventListener("touchmove", touchMove);
     };
   }, []);
 
   return mousePosition;
-}
+};
 
 export default useMousePosition;
